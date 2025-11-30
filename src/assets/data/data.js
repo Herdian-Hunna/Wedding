e8f6c0f05522f581adf95f06fea386a771ff7618
Herdian-Hunna <weddingjanuari118@gmail.com>
2025-11-30 14:33:12 +0700

    Update author credit in index.html and replace couple image with new JPG format

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -17,7 +17,7 @@ export const data = {
             image: './src/assets/images/cewe.png'
         },
 
-        couple: './src/assets/images/couple.png'
+        couple: './src/assets/images/couple.JPG'
     },
 
     time: {
